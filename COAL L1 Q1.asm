020702aedecd377fcd910bf9a0afac4bc00115fe
laibamhl2222-byte <laibamhl2222@gmail.com>
2026-02-14 18:07:52 +0500

    Add files via upload

---
+++ COAL L1 Q1.asm
@@ -0,0 +1,34 @@
+org 100h
+.data
+.code
+main proc
+    ; Print H
+    mov dl, 'H'
+    mov ah, 2
+    int 21h
+
+    ; Print E
+    mov dl, 'E'
+    mov ah, 2
+    int 21h
+
+    ; Print L
+    mov dl, 'L'
+    mov ah, 2
+    int 21h
+
+    ; Print L
+    mov dl, 'L'
+    mov ah, 2
+    int 21h
+
+    ; Print O
+    mov dl, 'O'
+    mov ah, 2
+    int 21h
+
+    ; End program
+    mov ah, 4Ch
+    int 21h
+main endp
+end main
